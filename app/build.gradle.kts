09f6782cb201c1be8c86288b4b7adcff8f01a9b7
Beka Modebadze <bexx.modd@gmail.com>
2022-12-16 21:16:32 -0800

    starter code for project

---
+++ app/build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile // For `KotlinCompile` task below
+
+plugins {
+    id("org.springframework.boot") version "2.7.1"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.7.21" // The version of Kotlin to use
+    kotlin("plugin.spring") version "1.7.21" // The Kotlin Spring plugin
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin") // Jackson extensions for Kotlin for working with JSON
+    implementation("org.jetbrains.kotlin:kotlin-reflect") // Kotlin reflection library, required for working with Spring
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8") // Kotlin standard library
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.2") // Kotling logging
+    runtimeOnly("com.h2database:h2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> { // Settings for `KotlinCompile` tasks
+    kotlinOptions { // Kotlin compiler options
+        freeCompilerArgs = listOf("-Xjsr305=strict") // `-Xjsr305=strict` enables the strict mode for JSR-305 annotations
+        jvmTarget = "17" // This option specifies the target version of the generated JVM bytecode
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
